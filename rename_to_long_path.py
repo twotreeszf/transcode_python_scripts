8de0af37970aa7c23b50e07b0a2d05e2b72c120e
zhangfan <twotrees.zf@gmail.com>
2020-11-17 22:42:17 +0800

    扩充文件格式

--- rename_to_long_path.py
+++ rename_to_long_path.py
@@ -10,12 +10,13 @@ import os
 import sys
 from os import path
 from video_formats import VIDEO_FORMATS
+from video_formats import PHOTO_FORMATS
 
 def processInFolder(folder):
     for (dir, dirNames, fileNames) in os.walk(folder):
         for fileName in fileNames:
             baseName, extName = path.splitext(fileName)
-            if extName.lower() in VIDEO_FORMATS:
+            if extName.lower() in VIDEO_FORMATS or extName.lower() in PHOTO_FORMATS:
                 filePath = path.join(dir, fileName)
                 relativePath = path.relpath(filePath, folder)
                 longFileName = relativePath.replace('/', '_')
--- video_formats.py
+++ video_formats.py
@@ -6,4 +6,5 @@ Date:       2016/08/17 16:16
 Desc:       
 """
 
-VIDEO_FORMATS = ['.mov', '.m4v', '.mp4', '.mts', '.mod', '.avi', '.wmv', '.3gp', '.mpg', '.mkv', '.ts', '.asf', '.flv']
+VIDEO_FORMATS = ['.mov', '.m4v', '.mp4', '.mts', '.mod', '.avi', '.wmv', '.3gp', '.mpg', '.mkv', '.ts', '.asf', '.flv', '.rmvb']
+PHOTO_FORMATS = ['.jpg', '.png']
