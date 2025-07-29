89b570287dbffd6ceebaddc41db796dd5620aa7a
Derek Shepherd <Derek.Shepherd@gmail.com>
2025-07-29 20:45:55 +0800

    Increase the bit rate

--- transcode_file.py
+++ transcode_file.py
@@ -15,19 +15,19 @@ import uuid
 Commands = {
 'VH265High_ACopy' : './ffmpeg -i {input} -c:a copy -async 1 ' \
                      '-c:v libx265 -profile:v main444-8 -preset veryslow ' \
-                     '-pix_fmt yuv420p -crf 18 -maxrate 4M -bufsize 4M -sn -threads {cpu_count} -y {output}',
+                     '-pix_fmt yuv420p -crf 18 -maxrate 8M -bufsize 8M -sn -threads {cpu_count} -y {output}',
 
 'VH265High_AAAC' : './ffmpeg -i {input} -c:a aac -async 1 ' \
                     '-c:v libx265 -profile:v main444-8 -preset veryslow ' \
-                    '-pix_fmt yuv420p -crf 18 -maxrate 4M -bufsize 4M -sn -threads {cpu_count} -y {output}',
+                    '-pix_fmt yuv420p -crf 18 -maxrate 8M -bufsize 8M -sn -threads {cpu_count} -y {output}',
 
 'VH264High_ACopy' : './ffmpeg -i {input} -c:a copy -async 1 ' \
                      '-c:v libx264 -profile:v high422 -preset veryslow ' \
-                     '-pix_fmt yuv420p -crf 18 -maxrate 4M -bufsize 4M -sn -threads {cpu_count} -y {output}',
+                     '-pix_fmt yuv420p -crf 18 -maxrate 8M -bufsize 8M -sn -threads {cpu_count} -y {output}',
 
 'VH264High_AAAC' : './ffmpeg -i {input} -c:a aac -async 1 ' \
                     '-c:v libx264 -profile:v high422 -preset veryslow ' \
-                    '-pix_fmt yuv420p -crf 18 -maxrate 4M -bufsize 4M -sn -threads {cpu_count} -y {output}',
+                    '-pix_fmt yuv420p -crf 18 -maxrate 8M -bufsize 8M -sn -threads {cpu_count} -y {output}',
 
 'VH264VeryHigh_AAAC' : './ffmpeg -i {input} -c:a aac -async 1 ' \
                     '-c:v libx264 -profile:v high422 -preset veryslow ' \
@@ -35,7 +35,7 @@ Commands = {
 
 'VH264HighGray_AAAC' : './ffmpeg -i {input} -c:a aac -async 1 ' \
                     '-c:v libx264 -profile:v high422 -preset veryslow ' \
-                    '-vf format=gray -pix_fmt yuv420p -crf 18 -maxrate 4M -bufsize 4M -sn -threads {cpu_count} -y {output}',
+                    '-vf format=gray -pix_fmt yuv420p -crf 18 -maxrate 8M -bufsize 8M -sn -threads {cpu_count} -y {output}',
 
 'VCopy_AAAC' : './ffmpeg -i {input} -c:a aac -async 1 -c:v copy -y {output}',
 
